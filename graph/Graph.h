92b5592c5fb2b90cd89e3f7173e3ac965a0f02bc
Subhajit Sahu <wolfram77@gmail.com>
2020-11-22 02:02:52 +0530

    :bus: start notebook plan

--- graph/Graph.h
+++ graph/Graph.h
@@ -1,6 +1,10 @@
 #pragma once
+#include <utility>
 #include "Table.h"
 #include "Staging.h"
+
+using std::pair;
+using std::make_pair;
 
 
 // Undirected graph is used when edges of a graph have no direction.
@@ -14,8 +18,17 @@ class Graph {
   GraphStaging<V, E>  staging;
 
   public:
+  pair<int, V> vertex(int i);
   bool isUpdated();
 };
+
+
+template <typename V, typename E>
+inline pair<int, V> Graph<V, E>::vertex(int i) {
+  auto v = table.vertex[i];
+  return {v.key, v.value};
+}
+
 
 template<typename V, typename E>
 inline bool Graph<V, E>::isUpdated() {
